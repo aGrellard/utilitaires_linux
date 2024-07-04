82eb10ad86c3947ecedacd764153995bb8b3f9f0
aGrellard <grellard.adrien@gmail.com>
2024-07-04 18:58:08 +0200

    upd

--- installation_linux/scripts/installation_paquets.sh
+++ installation_linux/scripts/installation_paquets.sh
@@ -14,7 +14,10 @@ echo "Téléchargement de oh-my-zsh"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o install_ohmyzsh.sh
 
 echo "Lancer l'installation de oh-my-zsh"
-RUNZSH=no sh install_ohmyzsh.sh
+export RUNZSH=no
+export CHSH=no
+export KEEP_ZSHRC=yes
+sh install_ohmyzsh.sh
 
 echo "Copie du fichier de configuration depuis le dossier parent"
 cp ../config/.zshrc ~/.zshrc
