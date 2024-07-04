b914a2192a7e475b76e71deeaa40cc6a4f0f0e67
aGrellard <grellard.adrien@gmail.com>
2024-07-04 19:12:40 +0200

    upd

--- installation_linux/scripts/installation_paquets.sh
+++ installation_linux/scripts/installation_paquets.sh
@@ -13,18 +13,18 @@ sudo apt install -y zsh
 echo "Téléchargement de oh-my-zsh"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o install_ohmyzsh.sh
 
+echo "Copie du fichier de configuration depuis le dossier parent"
+cp ../config/.zshrc ~/.zshrc
+
+sudo chsh -s "$(which zsh)" $USER
+
 echo "Lancer l'installation de oh-my-zsh"
 export RUNZSH=no
 export CHSH=no
 sh install_ohmyzsh.sh --unattended
 
-echo "Copie du fichier de configuration depuis le dossier parent"
-cp ../config/.zshrc ~/.zshrc
-
 echo "Nettoyage du script d'installation"
 rm install_ohmyzsh.sh
-
-sudo chsh -s "$(which zsh)" $USER
 
 echo "Installation du plugin zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
